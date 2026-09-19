d38cf2597e65f4f14266487327c5187f1e69c34e
You9123 <you9123chacon@gmail.com>
2026-09-19 02:01:36 -0600

    Feature/busqueda de juegos en el frontend (#2)
    * Cambio visual de la pagina principal donde se realizo la primer prueba
    
    * Activacion de la Query envolviendo todo el proyecto, que facilita el trafico de informacion y peticiones HTTP a la API
    
    * En clients.ts se define la URL fija para centralizar las direcciones del Backend y en juegos.ts se define la funcion para buscar los juegos y guardarlos en la Query
    
    * Configuracion del uso del boton para buscar juegos y como este funciona
    
    * Esta es la funcion que usa el BuscarJuegos.ts para justamente validar los caracteres minimos para buscar y guarda en Query
    
    * Definicion del resultado al buscar un juego y la informacion que se vaa desplegar

---
+++ Frontend/src/types/juego.ts
@@ -0,0 +1,9 @@
+export interface ResultadoJuego {
+  rawg_id: number;
+  titulo: string;
+  portada_url: string | null;
+  fecha_lanzamiento: string | null;
+  metacritic: number | null;
+  generos: string;
+  plataformas: string;
+}
