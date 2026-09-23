b6b2fce85f9fb7eb453ad175ec9d08a5bb13412e
You9123 <you9123chacon@gmail.com>
2026-09-23 00:08:32 -0600

    feat: Se agrega el BrowserRouter a main.tsx para comenzar a autilizar el react-router-dom y se agrega todo lo creado anteriormente a App.tsx.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,25 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 import { BuscarJuegos } from "./components/BuscarJuegos";
+import { MiBiblioteca } from "./components/MiBiblioteca";
 import "./App.css";
 
 function App() {
   return (
-    <main>
-      <h1>Mi Biblioteca de Videojuegos</h1>
-      <BuscarJuegos />
-    </main>
+    <>
+      <nav className="nav">
+        <NavLink to="/" end>
+          Buscar
+        </NavLink>
+        <NavLink to="/biblioteca">Mi biblioteca</NavLink>
+      </nav>
+      <main>
+        <h1>Mi Biblioteca de Videojuegos</h1>
+        <Routes>
+          <Route path="/" element={<BuscarJuegos />} />
+          <Route path="/biblioteca" element={<MiBiblioteca />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
